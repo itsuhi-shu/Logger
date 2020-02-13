c12a2eb34a2b8060b8e46aaa09a88ca1a87f3152
i_shu <zhouyifeig@gmail.com>
2020-02-13 16:11:24 +0900

    Create Logger.swift

---
+++ Logger.swift
@@ -0,0 +1,38 @@
+import Foundation
+import os
+
+func message<T>(_ message: T,
+                level: Logger.Level = Logger.level,
+                file: String = #file, function: String = #function, line: Int = #line) -> String? {
+    switch level {
+    case .verbose:
+        let fileName = (file as NSString).lastPathComponent
+        return """
+
+        [file:\(fileName)]:[line:\(line)]
+        [thread:\(Thread.current)]
+        \(message)
+        ===========================================
+        """
+    case .debug:
+        return "\(message)"
+    case .release:
+        return nil
+    }
+}
+
+func log(_ message: @autoclosure () -> String?) {
+    if let message = message() {
+        os_log("%@", message)
+    }
+}
+
+struct Logger {
+    enum Level {
+        case release
+        case debug
+        case verbose
+    }
+    
+    static var level: Level = .release
+}
